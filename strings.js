5bdf1cdad68d921a8c14b07bf0fd2a10ad7a3f8d
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-10-16 17:44:32 +0900

    #3.2 HTML Fragments part Two

--- strings.js
+++ strings.js
@@ -25,4 +25,21 @@ const addWelcome = () => {
   wrapper.innerHTML = div;
 };
 
-setTimeout(addWelcome, 2000);
+//setTimeout(addWelcome, 2000);
+
+// HTML Fragments example2
+
+const friends = ["me", "lynn", "dal", "mark"];
+
+const ul = document.createElement("ul");
+friends.forEach((friend) => ul.append(`<li>${friend}</li>`));
+wrapper.append(ul);
+
+// 이 방식으로 하면 html이 잘 안 나온다.
+//  잘 나오게 하기 위해선 수정을 해야 하는데, 코드가 더 길어진다.
+
+const list = `
+  <h1>friends</h1>
+  <ul>${friends.map((friend) => `<li>${friend}</li>`).join("")}</ul>
+`;
+wrapper.innerHTML = list;
