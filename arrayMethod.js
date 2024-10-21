1b2ec1f42a2f5ecaab4d3bc851c25a336924b123
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-10-21 19:54:13 +0900

    #4.1 Array.find() Array.findIndex() Array.fill()

--- arrayMethod.js
+++ arrayMethod.js
@@ -14,3 +14,31 @@ const buttons = document.getElementsByClassName("btn");
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", () => console.log("I've been clicked"));
 });
+
+// More Array Methods
+
+const friendsEmail = [
+  "nico@gmail.com",
+  "lynn@naver.com",
+  "dal@yahoo.com",
+  "flynn@gorea.com",
+  "mark@hotmail.com",
+];
+
+const check = () => friendsEmail.findIndex((email) => email.includes("gorea"));
+let target = check();
+
+if (target !== -1) {
+  /*   console.log(target);
+
+  const username = friendsEmail[target].split("@")[0];
+  const email = "korea.com";
+
+  friendsEmail[target] = `${username}@${email}`;
+
+  target = check(); */
+  friendsEmail.fill("*".repeat(5), target, target + 1);
+  console.log(friendsEmail);
+}
+
+console.log(target);
