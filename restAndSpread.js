dbaadf556f4cecfe63c9bd3d13be03e40aa9df30
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-10-24 17:51:58 +0900

    #6.0 Introduction to Spread

---
+++ restAndSpread.js
@@ -0,0 +1,8 @@
+// Spread
+const friends = [1, 2, 3, 4];
+const family = ["a", "b", "c"];
+
+console.log(friends);
+console.log(...friends);
+console.log([...friends, ...family]);
+console.log(friends.concat(family));
