4d522ca7865ec57114b5f11b8b0a156e72fabaf1
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-10-27 11:13:22 +0900

    #8.4 Promise.all

--- promise.js
+++ promise.js
@@ -1,4 +1,4 @@
-const amISexy = new Promise((resolve, reject) => {
+/* const amISexy = new Promise((resolve, reject) => {
   resolve(2);
 });
 
@@ -15,3 +15,20 @@ amISexy
   })
   .then((lastNumber) => console.log(lastNumber))
   .catch((error) => console.log(error));
+ */
+
+// Promise.all
+const p1 = new Promise((resolve) => {
+  setTimeout(resolve, 5000, "first");
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(reject, 1000, "second");
+});
+const p3 = new Promise((resolve) => {
+  setTimeout(resolve, 3000, "third");
+});
+
+const motherPromise = Promise.all([p1, p2, p3]);
+motherPromise
+  .then((values) => console.log(values))
+  .catch((err) => console.log(err));
