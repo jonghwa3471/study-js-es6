82cddc1441b0c5cd969199684a2a5aa0eff090d5
송종화 <whdghk3471@gmail.com>
2024-10-29 01:14:38 +0900

    #10.1 Extending Classes

--- classes.js
+++ classes.js
@@ -1,13 +1,37 @@
 class User {
-  constructor(name) {
+  constructor(name, lastname, email, password) {
     this.username = name;
+    this.lastname = lastname;
+    this.email = email;
+    this.password = password;
   }
-  sayHello() {
+  sayHello = () => {
     console.log(`Hi, I'm ${this.username}`);
+  };
+  getProfile = () => {
+    console.log(`${this.username} ${this.email} ${this.password}`);
+  };
+  updatePassword = (newPassword, currentPassword) => {
+    if (currentPassword === this.password) {
+      this.password = newPassword;
+    } else {
+      console.log("You've got wrong password");
+    }
+  };
+}
+
+const sexyUser = new User("Nicolas", "Serrano", "nico@com", "1234");
+
+class Admin extends User {
+  constructor(superAdmin) {
+    this.superAdmin = superAdmin;
+  }
+  deleteWebsite() {
+    console.log("Deleting the whole website...");
   }
 }
 
-const sexyUser = new User("Nico");
+const sexyAdmin = new Admin("Nicolas", "Serrano", "nico@com", "1234", true);
 
-console.log(sexyUser.username);
-setTimeout(() => sexyUser.sayHello(), 2000);
+sexyAdmin.deleteWebsite();
+console.log(sexyAdmin.email);
