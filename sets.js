119f7c5107acf26003fe378d6304610be2a5ae6b
송종화 <whdghk3471@gmail.com>
2024-10-29 20:57:34 +0900

    #11.3 WeakSet

--- sets.js
+++ sets.js
@@ -4,3 +4,15 @@ sexySet.add("wow");
 sexySet.add([2, 19, 39]);
 console.log(sexySet);
 console.log(sexySet.size);
+
+// WeakSet
+const weakSet = new WeakSet();
+
+const sexy = {
+  im: true,
+};
+
+weakSet.add(sexy);
+console.log(weakSet);
+weakSet.add({ hello: true });
+console.log(weakSet);
