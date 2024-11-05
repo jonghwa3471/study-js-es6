d9b0ecec6f91927b6010d134a782280c0bf29325
송종화 <whdghk3471@gmail.com>
2024-11-05 17:52:56 +0900

    #13.4 trim, trimStart, trimEnd

---
+++ es2020/trimStartAndTrimEnd.js
@@ -0,0 +1,8 @@
+console.log("           hi");
+
+console.log("           hi".trimStart());
+console.log("hi         ".trimEnd());
+
+console.log("           hi        ".trim());
+
+// white space(공백)를 모두 정리
