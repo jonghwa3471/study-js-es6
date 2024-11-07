355763955e070e8a32670ef644b4741428d22937
송종화 <whdghk3471@gmail.com>
2024-11-07 19:57:05 +0900

    #14.2 Logical AND Assignment

--- es2021/logicalAssignments.js
+++ es2021/logicalAssignments.js
@@ -8,3 +8,17 @@ let name = prompt("what is your name?");
 name ||= "anonymous";
 
 console.log(`Hello, ${name}`);
+
+// AND Operator
+const user = {
+  name: "nico",
+  password: 123,
+};
+
+/* if (user.password) {
+  user.password = "[secret]";
+} */
+
+user.password &&= "[secret]";
+
+console.log(user);
